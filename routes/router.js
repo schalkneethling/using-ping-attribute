b7b47af7de6de7b0531e07498455f1a257eed697
Schalk Neethling <sneethling@mozilla.com>
2021-04-28 19:58:51 +0200

    ping use case example app

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
+});
+
+router.get("/pinged", (req, res) => {
+  res.render("pinged");
+});
+
+router.post("/ping", (req, res) => {
+  console.log(`origin: ${req.get("ping-from")}`);
+  console.log(`destination: ${req.get("ping-to")}`);
 });
 
 module.exports = router;
